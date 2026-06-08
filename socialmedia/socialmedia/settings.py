1c92e0b65ad85e3ee2bad559d2d443aae80baebf
Your GitHub Username <your_github_email@example.com>
2026-06-09 02:31:48 +0530

    feat: initialize Django project settings and update Render configuration for deployment

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-x4n5uj&qkfkw-=&_n0lx6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = [host.strip() for host in os.environ.get("ALLOWED_HOSTS", "*").split(",") if host.strip()]
 
 if not DEBUG:
     if SECRET_KEY.startswith("django-insecure-"):
